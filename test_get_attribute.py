0d3dae156e26df6aff74abfd7dda5a8da1b1f2ae
Administrator <727991861@qq.com>
2021-11-24 17:36:15 +0800

    appium-get attribute

--- test_get_attribute.py
+++ test_get_attribute.py
@@ -1,10 +1,12 @@
 from time import sleep
 
+import pytest
 from appium import webdriver
 from appium.webdriver.common.mobileby import MobileBy
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
+from hamcrest import *
 
 
 class TestGetAttribute:
@@ -32,12 +34,22 @@ class TestGetAttribute:
     def teardown(self):
         self.driver.quit()
 
+    @pytest.mark.skip
     def test_get_attribute(self):
-
         search_ele = self.driver.find_element(By.ID, "com.xueqiu.android:id/tv_search")
         print(search_ele.get_attribute("content-desc"))
         print(search_ele.get_attribute("resource-id"))
         print(search_ele.get_attribute("enabled"))
         print(search_ele.get_attribute("clickable"))
         print(search_ele.get_attribute("bounds"))
+        assert 'search' in search_ele.get_attribute("enabled")
+
+    def test_hamcrest(self):
+        #equal_to:等于
+        assert_that(10,equal_to(10))
+        #close_to：10上下浮动2
+        assert_that(12,close_to(10,2))
+        #contains_string:包含字符串
+        assert_that("contains some string",contains_string("string"))
+
 
