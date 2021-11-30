f27db9943f960fc4be10f646ed18b7d03b074c13
Administrator <727991861@qq.com>
2021-11-30 17:42:51 +0800

    appium-微信小程序测试-测试用例没通过，不是太明白小程序测试这部分

--- appium_webview/test_weixin_xiaochengxu.py
+++ appium_webview/test_weixin_xiaochengxu.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from appium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
@@ -6,54 +8,33 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 class TestWeiXinXiaoChengXu:
     def setup(self):
-        caps = {
-            "platformName":"android",
-            "deviceName":"127.0.0.1:7555",
-            "appPackage":"com.tencent.mm",
-            "appActivity":"com.tencent.mm.ui.LauncherUI",
-            "noReset":True,
-            "unicodeKeyboard": True,
-            "resetKeyboard":True,
-
-            # 第一步：设置正确的chromedriver
-            "chromedriverExecutable":"G:/lxj/chromedriver.exe",
-            # 第二步：设置chromeoption传递给chromedriver,把小程序的进程名传递进去
-            #"androidProcess":"com.tencent.mm:appbrand0",
-            "goog:chromeOptions":{"androidProcess":"com.tencent.mm:appbrand0"},
-            "adbPort":5038
-
-        }
-
-        '''
-        caps["platformName"] = "android",
-        #caps["deviceName"] = "测试人社区 ceshiren.com"
-        caps["deviceName"] = "127.0.0.1:7555",
-        caps["appPackage"] = "com.tencent.mm",
-        caps["appActivity"] = "com.tencent.mm.ui.LauncherUI",
-        caps["noReset"] = True,
-        caps["unicodeKeyboard"] = True,
-        caps["resetKeyboard"] = True,
+        caps = {}
+        caps["platformName"] = "android"
+        caps["deviceName"] = "127.0.0.1:7555"
+        caps["appPackage"] = "com.tencent.mm"
+        caps["appActivity"] = "com.tencent.mm.ui.LauncherUI"
+        caps["noReset"] = True
+        caps["unicodeKeyboard"] = True
+        caps["resetKeyboard"] = True
 
         # 第一步：设置正确的chromedriver
-        caps["chromedriverExecutable"] = "G:/lxj/chromedriver.exe",
+        caps["chromedriverExecutable"] = "G:/lxj/chromedriver.exe"
 
+        # 第二步：设置chromeoption传递给chromedriver,把小程序的进程名传递进去
         # 因为小程序的进程名跟包名不一样，需要加上这个参数
         #ChromeOptions chromeOptions = new ChromeOptions(),
         #chromeOptions.setExperimentalOption(name: "androidProcess", value: "com.tencent.mm:appbrand0"),
         #desiredCapabilities.setCapability("goog:chromeOptions", chromeOptions),
-
-        # 第二步：设置chromeoption传递给chromedriver,把小程序的进程名传递进去
-        caps["chromeOptions"] = {"androidProcess":"com.tencent.mm:appbrand0"},
+        caps["chromeOptions"] = {"androidProcess":"com.tencent.mm:appbrand0"}
 
         # 第三步：使用adb proxy,fix chromedriver的BUG
-        # desiredCapabilities.setCapability(capabilityName:"adbPort",value:"5038"),
-        caps["adbPort"] = 5038,
-        '''
+        caps["adbPort"] = 5038
+
         self.driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub",caps)
         self.driver.implicitly_wait(30)
 
-        self.driver.find_element(By.XPATH,"//*[@text='通讯录']")
-        self.driver.implicitly_wait(10)
+        #self.driver.find_element(By.XPATH,"//*[@text='通讯录']")
+        #self.driver.implicitly_wait(10)
 
     def teardown(self):
         pass
@@ -61,12 +42,19 @@ class TestWeiXinXiaoChengXu:
     def test_search(self):
         #原生程序自动化
         size = self.driver.get_window_size()
-        self.driver.swipe(size['width']*0.5,size['height']*0.4,size['width']*0.5,size['height']*0.4)
+        print(size)
+        self.driver.swipe(size['width']*0.5,size['height']*0.3,size['width']*0.5,size['height']*0.9)
+        print("我已经下拉了页面")
         self.driver.find_element(By.CLASS_NAME,'android.widget.EditText').click()
-        self.driver.find_element(By.XPATH,"//*[@text='取消']")
+        #self.driver.find_element(By.XPATH,"//*[@text='取消']")
         self.driver.find_element(By.CLASS_NAME, 'android.widget.EditText').send_keys("雪球")
-        self.driver.find_element(By.CLASS_NAME, 'android.widget.Button')
-        self.driver.find_element(By.CLASS_NAME, 'android.widget.Button').click()
+        sleep(2)
+        #self.driver.find_element(By.CLASS_NAME, 'android.widget.Button')
+
+        class_text = 'className("android.widget.Button").text("雪球")'
+        self.driver.find_element_by_android_uiautomator(class_text).click()
+
+        #self.driver.find_element(By.CLASS_NAME, 'android.widget.Button').click()
         self.driver.find_element(By.XPATH,"//*[@text='自选']")
         print(self.driver.contexts)
 
